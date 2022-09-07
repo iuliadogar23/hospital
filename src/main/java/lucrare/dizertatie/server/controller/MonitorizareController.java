48a5abb06e6b625d44f2590d7d8a71a494bf1482
Iulia Dogar <iulia.dogar@p4b.ro>
2022-09-07 16:12:21 +0300

    changes

--- src/main/java/lucrare/dizertatie/server/controller/MonitorizareController.java
+++ src/main/java/lucrare/dizertatie/server/controller/MonitorizareController.java
@@ -2,11 +2,17 @@ package lucrare.dizertatie.server.controller;
 
 import io.swagger.annotations.Api;
 import lucrare.dizertatie.common.exception.MessagingException;
+import lucrare.dizertatie.server.model.FisaMedicala;
+import lucrare.dizertatie.server.service.FisaMedicalaService;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
@@ -21,21 +27,26 @@ import java.util.stream.Collectors;
 @Api("Monitorizare pacient")
 public class MonitorizareController {
 
+    @Autowired
+    FisaMedicalaService fisaMedicalaService;
+
     @PostMapping
-    @Transactional(timeout = 1)
+    @Transactional(timeout = 3)
     public ResponseEntity receiveEvents(@RequestBody String nrFisa){
-        try {
             JSONObject jsonObject = new JSONObject(nrFisa);
             Integer fisa = (Integer) jsonObject.get("pacientId");
-            var client = HttpClient.newHttpClient();
-            var request = HttpRequest.newBuilder().uri(URI.create("http://localhost:3333/simulator/subscribe?pacientId="+ fisa.toString())).GET().build();
-            var lines = client.send(request, HttpResponse.BodyHandlers.ofLines()).body();
-            lines.forEach(System.out::println);
-            System.out.println(lines.collect(Collectors.joining("\n")));
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw new MessagingException("Failed to receive event", e);
-        }
+            FisaMedicala fisaMedicala = fisaMedicalaService.findById(fisa);
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+
+            try {
+
+                restTemplate.getForEntity("http://localhost:3333/simulator/subscribe?pacientId="+ fisa.toString(), ResponseEntity.class);
+            } catch (Exception e) {
+
+            }
         return ResponseEntity.ok().build();
     }
 
